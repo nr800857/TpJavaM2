02b9a8d2e620679acf8993e46f23f52fa63aae8e
nr800857 <remyng06@gmail.com>
2023-01-31 16:17:17 +0100

    Avancer request mapping

--- my-finder/src/main/java/com/example/myfinder/controller/MyFinderApplicationController.java
+++ my-finder/src/main/java/com/example/myfinder/controller/MyFinderApplicationController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -46,6 +47,34 @@ public class MyFinderApplicationController {
         return response;
     }
 
+    @ApiOperation(value = "Avoir un film présent dans le système ", response = Object.class, tags = "getFilm")
+    @HystrixCommand(fallbackMethod = "callMyMovie_Fallback")
+    @RequestMapping(value = "/getFilm/{name}")
+    public String getFilms(@PathVariable(value = "name") String name) {
+        String response = restTemplate
+                .exchange("http://localhost:8010/getFilms/{name}"
+                        , HttpMethod.GET
+                        , null
+                        , new ParameterizedTypeReference<String>() {
+                        }, name).getBody();
+
+        return response;
+    }
+
+    @ApiOperation(value = "Avoir les film présent dans le système selon la date ", response = Iterable.class, tags = "getFilmByDate")
+    @HystrixCommand(fallbackMethod = "callMyMovie_Fallback")
+    @RequestMapping(value = "/getFilmByDate/{date}")
+    public String getFilmByDate(@PathVariable(value = "date") String date) {
+        String response = restTemplate
+                .exchange("http://localhost:8010/getFilmByDate/{date}"
+                        , HttpMethod.GET
+                        , null
+                        , new ParameterizedTypeReference<String>() {
+                        }, date).getBody();
+
+        return response;
+    }
+
     @ApiOperation(value = "Avoir la liste des films du ms my-movie ", response = Object.class, tags = "getActeurs")
     @HystrixCommand(fallbackMethod = "callMyMovie_Fallback")
     @RequestMapping(value = "/getActeurs")
@@ -59,4 +88,34 @@ public class MyFinderApplicationController {
 
         return response;
     }
+
+    @ApiOperation(value = "Avoir un acteur présent dans le système ", response = Acteur.class, tags = "getActeur")
+    @HystrixCommand(fallbackMethod = "callMyMovie_Fallback")
+    @RequestMapping(value = "/getActeur/{name}")
+    public String getActeur(@PathVariable(value = "name") String name) {
+        String response = restTemplate
+                .exchange("http://localhost:8010/getActeur/{name}"
+                        , HttpMethod.GET
+                        , null
+                        , new ParameterizedTypeReference<String>() {
+                        }, name).getBody();
+
+        return response;
+    }
+
+    @ApiOperation(value = "Avoir un acteur présent dans le système selon un film", response = Acteur.class, tags = "getActeurByFilm")
+    @HystrixCommand(fallbackMethod = "callMyMovie_Fallback")
+    @RequestMapping(value = "/getActeurByFilm/{name}")
+    public String getActeurByFilm(@PathVariable(value = "name") String name) {
+        String response = restTemplate
+                .exchange("http://localhost:8010/getActeurByFilm/{name}"
+                        , HttpMethod.GET
+                        , null
+                        , new ParameterizedTypeReference<String>() {
+                        }, name).getBody();
+
+        return response;
+    }
+
+
 }
